52be6f77401b84a6467f85abb46d9b6a7b1b144a
RHiaHoa <58160038@go.buu.ac.th>
2018-08-29 13:57:11 +0700

    add

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String input = JOptionPane.showInputDialog("ใส่ตัวเลข 1 ตัว","0");
+        JOptionPane.showMessageDialog(null,"message","tittle",JOptionPane.INFORMATION_MESSAGE);
+        int ans = JOptionPane.showConfirmDialog(null,"รักหรือเปล่า","ถามเธอ",JOptionPane.YES_NO_OPTION);
+        if(ans==JOptionPane.YES_NO_OPTION){
+            JOptionPane.showMessageDialog(null,"รักนะจุ๊บๆ");
+        }else {
+            JOptionPane.showMessageDialog(null,"จำไว้เลย");
+        }
+
+    }
+}
